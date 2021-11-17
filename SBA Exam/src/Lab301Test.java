25a743391e6e311fa8e162f784213fb3d4e3a912
Prince Felix <princeafelix@gmail.com>
2021-11-17 09:40:18 -0500

    SBA Added

---
+++ SBA Exam/src/Lab301Test.java
@@ -0,0 +1,16 @@
+
+public class Lab301Test {
+	
+	public static void menu() {
+		System.out.print("Please select one of the following:\n1. Add Student to College System\n"
+				+ "2. Display Students in the System \n"
+				+ "3. Set College System Name\n"
+				+ "4. Search for a Student\n"
+				+ "5. Exit\n>");
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
